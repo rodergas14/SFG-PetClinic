3a6ffd93678e716a787342805f5935d690615d21
rodergas14 <robertalmar@gmail.com>
2020-11-20 21:42:14 +0100

    Implement map pet type service. Closes #29.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetTypeMapService.java
@@ -3,10 +3,13 @@ package guru.springframework.petclinic.services.map;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.petclinic.model.Pet;
 import guru.springframework.petclinic.model.PetType;
 import guru.springframework.petclinic.services.PetTypeService;
 
+@Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService{
 
 	@Override
