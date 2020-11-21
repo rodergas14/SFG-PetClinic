42c4d57709f98b116dbb83d97e1a04095c495964
rodergas14 <robertalmar@gmail.com>
2020-11-21 19:07:44 +0100

    Added JPA to base Entity

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -2,10 +2,17 @@ package guru.springframework.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = -6852319477180547150L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
