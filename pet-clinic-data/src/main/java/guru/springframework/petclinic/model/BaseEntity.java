fbd9400bfa95183095d2a27d089408fd29762374
rodergas14 <robertalmar@gmail.com>
2020-11-21 19:21:30 +0100

    update owner with JPA entity mapping. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -2,6 +2,7 @@ package guru.springframework.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
